57384fc5635facada61e801b1864f9af651ebed9
Chase Simmons <csim6890@gmail.com>
2020-11-06 15:42:19 -0600

    added a limiter on ranking

--- src/pages/Feeling/Feeling.js
+++ src/pages/Feeling/Feeling.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 
 class Feeling extends Component {
   state = {
-    inputValue : 1;
-  }
+    inputValue: 1,
+  };
   onSubmit = (event) => {
     console.log(event.target);
+  };
+  onInputChange = (event) => {
+    if (event.target.value > 5) {
+      this.setState({
+        inputValue: 5,
+      });
+    } else if (event.target.value < 1) {
+      this.setState({
+        inputValue: 1,
+      });
+    } else {
+      this.setState({
+        inputValue: event.target.value,
+      });
+    }
   };
   render() {
     return (
@@ -13,7 +28,11 @@ class Feeling extends Component {
         <h3>How are you feeling today?</h3>
         <p>Feeling?</p>
         <form onSubmit={this.onSubmit} display="inline">
-          <input type="number" value={this.state.inputValue} onChange ></input>
+          <input
+            type="number"
+            value={this.state.inputValue}
+            onChange={this.onInputChange}
+          ></input>
           <button>Next</button>
         </form>
       </div>
